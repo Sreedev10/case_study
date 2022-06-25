7b39ee6d040b4d738742fa7a25f9beff61ff22ea
Sreedev10 <sreedevskurup10@gmail.com>
2022-06-25 00:02:51 -0700

    completed

---
+++ contact.js
@@ -0,0 +1,16 @@
+contactBtnEvent=()=>{
+    name=(document.getElementById("name").value)
+    message=(document.getElementById("message").value)
+    phone=(document.getElementById("phone").value)
+
+    console.log(name)
+    alert(name)
+
+    console.log(message)
+    alert(message)
+
+    console.log(phone)
+    alert(phone)
+
+
+}
